c154f0eabf2e669e456e9d193f9e5ef74896dd75
aolikas <olkaspark@gmail.com>
2020-11-18 17:54:53 +0100

    a sketch sending data from soil sensor to telegram bot with deep sleep

---
+++ sketch_telegram_push/sketch_telegram_push.ino
@@ -0,0 +1,103 @@
+#include <UniversalTelegramBot.h>
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+#include <ArduinoJson.h>
+
+// Wifi defenitions
+const char* ssid = "TrollAthenaeum";
+const char* password = "PendeL100kg";
+
+//Telegram config
+#define BOTtoken "1400009535:AAEub-QSHVMloGPZ90l7xiHEFmoAPX9FYiM"
+#define CHAT_ID "602718839"
+
+WiFiClientSecure net_ssl; 
+UniversalTelegramBot bot(BOTtoken, net_ssl);
+
+const int AirValue = 857;
+const int WaterValue = 440;
+int intervals = (AirValue - WaterValue) / 3;
+
+void setup() {
+  Serial.begin(115200);
+  Serial.setTimeout(2000);
+
+  //wait for serial to init
+  while(!Serial) {}
+
+  net_ssl.setInsecure();
+
+  //init sensor
+  pinMode(A0, INPUT);
+
+  connectToWiFi();
+
+  //bot.sendMessage(CHAT_ID, "Bot is starting", "");
+
+  sendToTelegram();
+
+  Serial.println("Going to sleep");
+
+  ESP.deepSleep(3600e6);
+
+}
+
+void loop() {
+ 
+}
+
+void sendToTelegram() {
+  String sensorData = getReadings();
+  
+  if(sensorData.equals("Dry")) {
+    bot.sendMessage(CHAT_ID, "Sensor is " + sensorData + ", watering your plant!", "");
+    Serial.println(sensorData);
+  }
+}
+
+void connectToWiFi() {
+
+  //set WiFi to station mode and disconnect from AP if 
+  // it was previously connected
+   WiFi.mode(WIFI_STA);
+   WiFi.disconnect();
+   delay(100);
+
+   // attempt to connect to WIFI network
+   Serial.print("Connected WiFi: ");
+   Serial.println(ssid);
+   WiFi.begin(ssid, password);
+   while(WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+   }
+   Serial.println("");
+   Serial.println("WiFi connected");
+   Serial.println("IP Address: ");
+   // Print ESP32 Local IP Address
+  Serial.println(WiFi.localIP()); 
+ 
+   }
+
+String getReadings() {
+  int soilMoistureValue = analogRead(A0);
+  String condition = "";
+
+  if(soilMoistureValue > WaterValue && 
+  soilMoistureValue < (WaterValue + intervals)) {
+    condition = "Very Wet";
+  } else if(soilMoistureValue > (WaterValue + intervals) &&
+  soilMoistureValue < (AirValue - intervals)) {
+    condition = "Wet"; 
+  } else if(soilMoistureValue < AirValue && 
+  soilMoistureValue > (AirValue - intervals)) {
+    condition = "Dry";
+  }
+
+  return condition;
+}
+
+
+
+    
+   
